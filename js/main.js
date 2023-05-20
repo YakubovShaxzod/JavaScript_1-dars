4ba92c9545162ea3aaa3bb12f2f330cf2a9b4eb2
YakubovShaxzod <133785760+YakubovShaxzod@users.noreply.github.com>
2023-05-20 15:17:56 +0500

    JavaScript 1-dars
    Bu yerda JavaScript 1-dars kodlari yukandi

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*console.log(12*12);*/
+
+/*alert("Добро пожаловать!");*/
+
+/*document.write("Men body oynasida korinaman!")*/
+
+// let x = 2;
+// let y = 5;
+// let salom = "Dobro Pojalovat"
+// let color = "#f1f1f1"
+// let x = 2, y = 5, z = 10;
+// document.write(x+y+z);
+//
+// J5 data tyypes
+//
+// let x = 5;
+// let matn = "JavaScript"
+// document.write(typeof matn);
+
+/*JSda orifmetik amallar*/
+// x+=6; /* x = 5 + 6 = 11*/
+// x-=6; /* x = 5 - 6 = -1*/
+// x*=60; /* x = 5 * 60 = 300*/
+// x/=2; /* x = 5 / 2 = 2.5*/
+// let qoldiq = x% 19;
+// let natija = x**5;/* 50*50*50*50*50*/
+// ++x 1 ta qowib beradi;
+// x-- 1 ta ob tawidi;
+// x--;
+
+// document.write(x--)
+// let son = 5;
+// let matnson = "55";
